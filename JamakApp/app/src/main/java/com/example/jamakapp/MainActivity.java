3ffc118031efb4a9c6b1866dbe0d6343dae60d02
vishmum <vishalm713@gmail.com>
2021-01-19 22:15:30 -0500

    Shell python script

--- JamakApp/app/src/main/java/com/example/jamakapp/MainActivity.java
+++ JamakApp/app/src/main/java/com/example/jamakapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.jamakapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- Linux Scripts/plugin.py
+++ Linux Scripts/plugin.py
@@ -1,6 +1,47 @@
 import subprocess
+import time
 
 date = "2021-01-18 22:33:59.197"
+
+#result = subprocess.run([sys.executable, "-c", "print('ocean')"])
+# while True:
+
+#com.example.jamakapp/com.example.jamakapp.MainActivity
+
+#adb shell am start -n com.example.jamakapp/com.example.jamakapp.MainActivity
+
+
+# result = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE, text=True).stdout
+# result = result.splitlines()
+# print(result[1])
+# print(len(result[1]))
+#adb install app-debug.apk
+
+while True:
+    devices = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE, text=True).stdout
+    devices = devices.splitlines()
+    if len(devices[1]) > 0:
+        print ("Device Found: ", devices[1].split()[0])
+        break
+    else:
+        print ("Searching...")
+        time.sleep(.500)
+        continue
+
+print("\n")
+
+app = subprocess.run(["adb", "shell", "pm", "list", "packages", "|", "grep", "com.example.jamakapp"], stdout=subprocess.PIPE, text=True).stdout
+
+if len(app) == 0:
+    print("App not found. Downloading Jamak app...")
+    out = subprocess.run(["adb", "install", "app-debug.apk"], stdout=subprocess.PIPE, text=True).stdout
+    print(out)
+else:
+    print("App found")
+print("\nOpening app")
+openApp = subprocess.run(["adb", "shell", "am", "start", "-n", "com.example.jamakapp/com.example.jamakapp.MainActivity"], stdout=subprocess.PIPE, text=True).stdout
+print(openApp)
+
 while True:
     get_logs = subprocess.run(["adb", "logcat", "-d", "-v", "tag,time,year", "-t", date, "JamakApp:D", "*:S"], stdout=subprocess.PIPE, text=True).stdout
     last_log = get_logs.splitlines()[-1]
@@ -9,7 +50,7 @@ while True:
         date = last_log[0:index-1]
         # increase timestamp by 1 millisecond
         next_millisecond = int(date[-3:]) + 1
-        # if 
+        # if
         print(date[-3:])
         print(str(next_millisecond))
         # date[-3:] = str(next_millisecond)
