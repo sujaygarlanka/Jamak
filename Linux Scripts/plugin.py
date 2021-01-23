4e9a09aba08b04aa2989e4107d2b0a15add4a069
vishmum <vishalm713@gmail.com>
2021-01-23 15:07:27 -0500

    plugin.py

--- Linux Scripts/plugin.py
+++ Linux Scripts/plugin.py
@@ -1,22 +1,8 @@
 import subprocess
 import time
 
-date = "2021-01-18 22:33:59.197"
 
-#result = subprocess.run([sys.executable, "-c", "print('ocean')"])
-# while True:
-
-#com.example.jamakapp/com.example.jamakapp.MainActivity
-
-#adb shell am start -n com.example.jamakapp/com.example.jamakapp.MainActivity
-
-
-# result = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE, text=True).stdout
-# result = result.splitlines()
-# print(result[1])
-# print(len(result[1]))
-#adb install app-debug.apk
-
+print ("Searching for phone...")
 while True:
     devices = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE, text=True).stdout
     devices = devices.splitlines()
@@ -24,7 +10,6 @@ while True:
         print ("Device Found: ", devices[1].split()[0])
         break
     else:
-        print ("Searching...")
         time.sleep(.500)
         continue
 
@@ -42,19 +27,68 @@ print("\nOpening app")
 openApp = subprocess.run(["adb", "shell", "am", "start", "-n", "com.example.jamakapp/com.example.jamakapp.MainActivity"], stdout=subprocess.PIPE, text=True).stdout
 print(openApp)
 
+
+#date = "2021-01-18 22:33:59.197"
+#Wed Jan 20 21:48:35 EST 2021
+currDate = subprocess.run(["adb", "logcat","-d","-v","tag,time,year", "-t", "1"], stdout=subprocess.PIPE, text=True).stdout
+print (currDate)
+currDate = currDate.splitlines()[1].split()
+
+# print (currDate[0])
+# print (currDate[1])
+date = currDate[0] + " " + currDate[1]
+
+
+print("Searching...")
 while True:
+    #print("*****")
     get_logs = subprocess.run(["adb", "logcat", "-d", "-v", "tag,time,year", "-t", date, "JamakApp:D", "*:S"], stdout=subprocess.PIPE, text=True).stdout
-    last_log = get_logs.splitlines()[-1]
-    index = last_log.find("D/JamakApp")
-    if (index > -1):
-        date = last_log[0:index-1]
-        # increase timestamp by 1 millisecond
-        next_millisecond = int(date[-3:]) + 1
-        # if
-        print(date[-3:])
-        print(str(next_millisecond))
-        # date[-3:] = str(next_millisecond)
-    if "unformat" in last_log:
-        print("unformat")
-    elif "format" in last_log:
-        print("format")
+    #print("log " + get_logs + " " + date)
+    time.sleep(.500)
+    indexLogs = get_logs.splitlines()
+    #print (len(indexLogs))
+    if len(indexLogs) > 1:
+        # print("beg")
+        if indexLogs[-1][0] == '-':
+            continue
+        else:
+            # print(indexLogs[-1][0])
+            # print(indexLogs)
+            action = indexLogs[-1].split()[-1]
+            newDate = indexLogs[-1]
+            newDate = newDate.split()
+            #print(newDate)
+            date = newDate[0] + " " + newDate[1]
+            date = date[:-3] + str(int(date[-3:]) + 1)
+
+            if action == "format":
+                print("format")
+                subprocess.run(["adb", "shell", "wm", "density", "240"], stdout=subprocess.PIPE, text=True).stdout
+                #subprocess.run(["adb", "shell", "wm", "size", "1020x1900"], stdout=subprocess.PIPE, text=True).stdout
+                subprocess.run(["adb", "shell", "content", "insert", "--uri","content://settings/system","--bind","name:s:user_rotation", "--bind value:i:1"], stdout=subprocess.PIPE, text=True).stdout
+                subprocess.run(["adb", "shell", "content", "insert", "--uri","content://settings/system","--bind","name:s:accelerometer_rotation", "--bind value:i:0"], stdout=subprocess.PIPE, text=True).stdout
+
+            if action == "unformat":
+                print("unformat")
+                subprocess.run(["adb", "shell", "wm", "density", "reset"], stdout=subprocess.PIPE, text=True).stdout
+                subprocess.run(["adb", "shell", "wm", "size", "reset"], stdout=subprocess.PIPE, text=True).stdout
+                subprocess.run(["adb", "shell", "content", "insert", "--uri","content://settings/system","--bind","name:s:accelerometer_rotation", "--bind value:i:1"], stdout=subprocess.PIPE, text=True).stdout
+
+
+#
+# while True:
+#     get_logs = subprocess.run(["adb", "logcat", "-d", "-v", "tag,time,year", "-t", date, "JamakApp:D", "*:S"], stdout=subprocess.PIPE, text=True).stdout
+#     last_log = get_logs.splitlines()[-1]
+#     index = last_log.find("D/JamakApp")
+#     if (index > -1):
+#         date = last_log[0:index-1]
+#         # increase timestamp by 1 millisecond
+#         next_millisecond = int(date[-3:]) + 1
+#         # if
+#         print(date[-3:])
+#         print(str(next_millisecond))
+#         # date[-3:] = str(next_millisecond)
+#     if "unformat" in last_log:
+#         print("unformat")
+#     elif "format" in last_log:
+#         print("format")
